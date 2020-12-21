4fdae789216d158fdb99010659f0f1f68e436717
Igor <Kvait@bk.ru>
2020-12-21 14:18:05 +0300

    v 1.02

--- app/src/main/java/ru/game/bj21/MainActivity.kt
+++ app/src/main/java/ru/game/bj21/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         takeCard.setOnClickListener {
             theCard()
+            if(player1.score > 21 || player1.score == 21) {
+                takeCard.isEnabled = false
+            }
             counter.text = player1.score.toString()
         }
 
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             dealer.score = 0
             player1 = Player(cards)
             dealer = Player(cards)
+            takeCard.isEnabled = true
             resultGame.setBackgroundResource(R.drawable.lets_play)
             dealerStatus.setBackgroundResource(R.drawable.dealer_wait)
             result.visibility = View.GONE
@@ -58,25 +62,24 @@ class MainActivity : AppCompatActivity() {
         resultGame.visibility = View.VISIBLE
 
         if (myScore > 21 && dealerScore > 21) {
-            toast("Ничья! У обоих перебор.")
             resultGame.setBackgroundResource(R.drawable.draw)
 
         } else if (myScore in 21 downTo 16 && dealerScore in 21 downTo 16) {
             if (myScore > dealerScore) {
-                toast("Вы Победили!!!")
+
                 resultGame.setBackgroundResource(R.drawable.you_win)
             } else if (myScore == dealerScore) {
-                toast("Ничья")
+
                 resultGame.setBackgroundResource(R.drawable.draw)
             } else {
-                toast("Диллер Победил!!!")
+
                 resultGame.setBackgroundResource(R.drawable.you_loose)
             }
         } else if (myScore in 21 downTo 16 && dealerScore > 21) {
-            toast("Вы Победили!!!")
+
             resultGame.setBackgroundResource(R.drawable.you_win)
         } else if (dealerScore in 21 downTo 16 && myScore > 21 || myScore < 16) {
-            toast("Диллер Победил!!!")
+
             resultGame.setBackgroundResource(R.drawable.you_loose)
         } else {
             toast("ОЙ! Что-то не то))")
@@ -85,91 +88,91 @@ class MainActivity : AppCompatActivity() {
 
     private fun theCard() {
         val card = player1.takeCard()
-        result.visibility = View.VISIBLE
-        resultGame.visibility = View.GONE
+            result.visibility = View.VISIBLE
+            resultGame.visibility = View.GONE
 
-        when (card) {
-            "cross_11" -> result.setBackgroundResource(R.drawable.cross_ace)
-            "cross_6" -> result.setBackgroundResource(R.drawable.cross_six)
-            "cross_7" -> result.setBackgroundResource(R.drawable.cross_seven)
-            "cross_8" -> result.setBackgroundResource(R.drawable.cross_eight)
-            "cross_9" -> result.setBackgroundResource(R.drawable.cross_nine)
-            "cross_10" -> result.setBackgroundResource(R.drawable.cross_ten)
-            "cross_2" -> result.setBackgroundResource(R.drawable.cross_jack)
-            "cross_3" -> result.setBackgroundResource(R.drawable.cross_queen)
-            "cross_4" -> result.setBackgroundResource(R.drawable.cross_king)
+            when (card) {
+                "cross_11" -> result.setBackgroundResource(R.drawable.cross_ace)
+                "cross_6" -> result.setBackgroundResource(R.drawable.cross_six)
+                "cross_7" -> result.setBackgroundResource(R.drawable.cross_seven)
+                "cross_8" -> result.setBackgroundResource(R.drawable.cross_eight)
+                "cross_9" -> result.setBackgroundResource(R.drawable.cross_nine)
+                "cross_10" -> result.setBackgroundResource(R.drawable.cross_ten)
+                "cross_2" -> result.setBackgroundResource(R.drawable.cross_jack)
+                "cross_3" -> result.setBackgroundResource(R.drawable.cross_queen)
+                "cross_4" -> result.setBackgroundResource(R.drawable.cross_king)
 
-            "diamonds_11" -> result.setBackgroundResource(R.drawable.diamonds_ace)
-            "diamonds_6" -> result.setBackgroundResource(R.drawable.diamonds_six)
-            "diamonds_7" -> result.setBackgroundResource(R.drawable.diamonds_seven)
-            "diamonds_8" -> result.setBackgroundResource(R.drawable.diamonds_eight)
-            "diamonds_9" -> result.setBackgroundResource(R.drawable.diamonds_nine)
-            "diamonds_10" -> result.setBackgroundResource(R.drawable.diamonds_ten)
-            "diamonds_2" -> result.setBackgroundResource(R.drawable.diamonds_jack)
-            "diamonds_3" -> result.setBackgroundResource(R.drawable.diamonds_queen)
-            "diamonds_4" -> result.setBackgroundResource(R.drawable.diamonds_king)
+                "diamonds_11" -> result.setBackgroundResource(R.drawable.diamonds_ace)
+                "diamonds_6" -> result.setBackgroundResource(R.drawable.diamonds_six)
+                "diamonds_7" -> result.setBackgroundResource(R.drawable.diamonds_seven)
+                "diamonds_8" -> result.setBackgroundResource(R.drawable.diamonds_eight)
+                "diamonds_9" -> result.setBackgroundResource(R.drawable.diamonds_nine)
+                "diamonds_10" -> result.setBackgroundResource(R.drawable.diamonds_ten)
+                "diamonds_2" -> result.setBackgroundResource(R.drawable.diamonds_jack)
+                "diamonds_3" -> result.setBackgroundResource(R.drawable.diamonds_queen)
+                "diamonds_4" -> result.setBackgroundResource(R.drawable.diamonds_king)
 
-            "spades_11" -> result.setBackgroundResource(R.drawable.spades_ace)
-            "spades_6" -> result.setBackgroundResource(R.drawable.spades_six)
-            "spades_7" -> result.setBackgroundResource(R.drawable.spades_seven)
-            "spades_8" -> result.setBackgroundResource(R.drawable.spades_eight)
-            "spades_9" -> result.setBackgroundResource(R.drawable.spades_nine)
-            "spades_10" -> result.setBackgroundResource(R.drawable.spades_ten)
-            "spades_2" -> result.setBackgroundResource(R.drawable.spades_jack)
-            "spades_3" -> result.setBackgroundResource(R.drawable.spades_gueen)
-            "spades_4" -> result.setBackgroundResource(R.drawable.spades_king)
+                "spades_11" -> result.setBackgroundResource(R.drawable.spades_ace)
+                "spades_6" -> result.setBackgroundResource(R.drawable.spades_six)
+                "spades_7" -> result.setBackgroundResource(R.drawable.spades_seven)
+                "spades_8" -> result.setBackgroundResource(R.drawable.spades_eight)
+                "spades_9" -> result.setBackgroundResource(R.drawable.spades_nine)
+                "spades_10" -> result.setBackgroundResource(R.drawable.spades_ten)
+                "spades_2" -> result.setBackgroundResource(R.drawable.spades_jack)
+                "spades_3" -> result.setBackgroundResource(R.drawable.spades_gueen)
+                "spades_4" -> result.setBackgroundResource(R.drawable.spades_king)
 
-            "hearts_11" -> result.setBackgroundResource(R.drawable.hearts_ace)
-            "hearts_6" -> result.setBackgroundResource(R.drawable.hearts_six)
-            "hearts_7" -> result.setBackgroundResource(R.drawable.hearts_seven)
-            "hearts_8" -> result.setBackgroundResource(R.drawable.hearts_eight)
-            "hearts_9" -> result.setBackgroundResource(R.drawable.hearts_nine)
-            "hearts_10" -> result.setBackgroundResource(R.drawable.hearts_ten)
-            "hearts_2" -> result.setBackgroundResource(R.drawable.hearts_jack)
-            "hearts_3" -> result.setBackgroundResource(R.drawable.hearts_queen)
-            "hearts_4" -> result.setBackgroundResource(R.drawable.hearts_king)
+                "hearts_11" -> result.setBackgroundResource(R.drawable.hearts_ace)
+                "hearts_6" -> result.setBackgroundResource(R.drawable.hearts_six)
+                "hearts_7" -> result.setBackgroundResource(R.drawable.hearts_seven)
+                "hearts_8" -> result.setBackgroundResource(R.drawable.hearts_eight)
+                "hearts_9" -> result.setBackgroundResource(R.drawable.hearts_nine)
+                "hearts_10" -> result.setBackgroundResource(R.drawable.hearts_ten)
+                "hearts_2" -> result.setBackgroundResource(R.drawable.hearts_jack)
+                "hearts_3" -> result.setBackgroundResource(R.drawable.hearts_queen)
+                "hearts_4" -> result.setBackgroundResource(R.drawable.hearts_king)
 
-            "spades_ace" -> result.setBackgroundResource(R.drawable.spades_ace)
-            "spades_six" -> result.setBackgroundResource(R.drawable.spades_six)
-            "spades_seven" -> result.setBackgroundResource(R.drawable.spades_seven)
-            "spades_eight" -> result.setBackgroundResource(R.drawable.spades_eight)
-            "spades_nine" -> result.setBackgroundResource(R.drawable.spades_nine)
-            "spades_ten" -> result.setBackgroundResource(R.drawable.spades_ten)
-            "spades_jack" -> result.setBackgroundResource(R.drawable.spades_jack)
-            "spades_queen" -> result.setBackgroundResource(R.drawable.spades_gueen)
-            "spades_king" -> result.setBackgroundResource(R.drawable.spades_king)
+                "spades_ace" -> result.setBackgroundResource(R.drawable.spades_ace)
+                "spades_six" -> result.setBackgroundResource(R.drawable.spades_six)
+                "spades_seven" -> result.setBackgroundResource(R.drawable.spades_seven)
+                "spades_eight" -> result.setBackgroundResource(R.drawable.spades_eight)
+                "spades_nine" -> result.setBackgroundResource(R.drawable.spades_nine)
+                "spades_ten" -> result.setBackgroundResource(R.drawable.spades_ten)
+                "spades_jack" -> result.setBackgroundResource(R.drawable.spades_jack)
+                "spades_queen" -> result.setBackgroundResource(R.drawable.spades_gueen)
+                "spades_king" -> result.setBackgroundResource(R.drawable.spades_king)
 
-            "hearts_ace" -> result.setBackgroundResource(R.drawable.hearts_ace)
-            "hearts_six" -> result.setBackgroundResource(R.drawable.hearts_six)
-            "hearts_seven" -> result.setBackgroundResource(R.drawable.hearts_seven)
-            "hearts_eight" -> result.setBackgroundResource(R.drawable.hearts_eight)
-            "hearts_nine" -> result.setBackgroundResource(R.drawable.hearts_nine)
-            "hearts_ten" -> result.setBackgroundResource(R.drawable.hearts_ten)
-            "hearts_jack" -> result.setBackgroundResource(R.drawable.hearts_jack)
-            "hearts_queen" -> result.setBackgroundResource(R.drawable.hearts_queen)
-            "hearts_king" -> result.setBackgroundResource(R.drawable.hearts_king)
+                "hearts_ace" -> result.setBackgroundResource(R.drawable.hearts_ace)
+                "hearts_six" -> result.setBackgroundResource(R.drawable.hearts_six)
+                "hearts_seven" -> result.setBackgroundResource(R.drawable.hearts_seven)
+                "hearts_eight" -> result.setBackgroundResource(R.drawable.hearts_eight)
+                "hearts_nine" -> result.setBackgroundResource(R.drawable.hearts_nine)
+                "hearts_ten" -> result.setBackgroundResource(R.drawable.hearts_ten)
+                "hearts_jack" -> result.setBackgroundResource(R.drawable.hearts_jack)
+                "hearts_queen" -> result.setBackgroundResource(R.drawable.hearts_queen)
+                "hearts_king" -> result.setBackgroundResource(R.drawable.hearts_king)
 
-            "cross_ace" -> result.setBackgroundResource(R.drawable.cross_ace)
-            "cross_six" -> result.setBackgroundResource(R.drawable.cross_six)
-            "cross_seven" -> result.setBackgroundResource(R.drawable.cross_seven)
-            "cross_eight" -> result.setBackgroundResource(R.drawable.cross_eight)
-            "cross_nine" -> result.setBackgroundResource(R.drawable.cross_nine)
-            "cross_ten" -> result.setBackgroundResource(R.drawable.cross_ten)
-            "cross_jack" -> result.setBackgroundResource(R.drawable.cross_jack)
-            "cross_queen" -> result.setBackgroundResource(R.drawable.cross_queen)
-            "cross_king" -> result.setBackgroundResource(R.drawable.cross_king)
+                "cross_ace" -> result.setBackgroundResource(R.drawable.cross_ace)
+                "cross_six" -> result.setBackgroundResource(R.drawable.cross_six)
+                "cross_seven" -> result.setBackgroundResource(R.drawable.cross_seven)
+                "cross_eight" -> result.setBackgroundResource(R.drawable.cross_eight)
+                "cross_nine" -> result.setBackgroundResource(R.drawable.cross_nine)
+                "cross_ten" -> result.setBackgroundResource(R.drawable.cross_ten)
+                "cross_jack" -> result.setBackgroundResource(R.drawable.cross_jack)
+                "cross_queen" -> result.setBackgroundResource(R.drawable.cross_queen)
+                "cross_king" -> result.setBackgroundResource(R.drawable.cross_king)
 
-            "diamonds_ace" -> result.setBackgroundResource(R.drawable.diamonds_ace)
-            "diamonds_six" -> result.setBackgroundResource(R.drawable.diamonds_six)
-            "diamonds_seven" -> result.setBackgroundResource(R.drawable.diamonds_seven)
-            "diamonds_eight" -> result.setBackgroundResource(R.drawable.diamonds_eight)
-            "diamonds_nine" -> result.setBackgroundResource(R.drawable.diamonds_nine)
-            "diamonds_ten" -> result.setBackgroundResource(R.drawable.diamonds_ten)
-            "diamonds_jack" -> result.setBackgroundResource(R.drawable.diamonds_jack)
-            "diamonds_queen" -> result.setBackgroundResource(R.drawable.diamonds_queen)
-            "diamonds_king" -> result.setBackgroundResource(R.drawable.diamonds_king)
+                "diamonds_ace" -> result.setBackgroundResource(R.drawable.diamonds_ace)
+                "diamonds_six" -> result.setBackgroundResource(R.drawable.diamonds_six)
+                "diamonds_seven" -> result.setBackgroundResource(R.drawable.diamonds_seven)
+                "diamonds_eight" -> result.setBackgroundResource(R.drawable.diamonds_eight)
+                "diamonds_nine" -> result.setBackgroundResource(R.drawable.diamonds_nine)
+                "diamonds_ten" -> result.setBackgroundResource(R.drawable.diamonds_ten)
+                "diamonds_jack" -> result.setBackgroundResource(R.drawable.diamonds_jack)
+                "diamonds_queen" -> result.setBackgroundResource(R.drawable.diamonds_queen)
+                "diamonds_king" -> result.setBackgroundResource(R.drawable.diamonds_king)
 
-            else -> result.setBackgroundResource(R.drawable.draw)
-        }
+                else -> result.setBackgroundResource(R.drawable.draw)
+            }
     }
 }
